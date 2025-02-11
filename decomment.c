7116f4a9b3d449ca5cef972b74339d1ab0459c42
tomdubnov <td5828@princeton.edu>
2025-02-11 16:15:45 -0500

    ovid9

--- decomment.c
+++ decomment.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <ctype.h>
 
-
+/* Listing the possible states in DFA to be handled */
 enum Statetype {normal_text, 
    string_literal, 
    esc_char_from_string_literal, 
@@ -63,7 +63,7 @@ enum Statetype handle_char_literal(int currentchar) {
    enum Statetype state;
    if (currentchar == '\'') {
       putchar(currentchar); /*print quote*/
-      state = normal_text; /*end of char literal, return to normal text*/
+      state = normal_text; /*end of char literal, return to norm text*/
    } else if (currentchar == '\\') {
       putchar(currentchar);
       state = esc_char_from_char_literal;
@@ -120,7 +120,6 @@ enum Statetype handle_in_comment(int currentchar) {
    } else {
       state = in_comment;
    }
-
    return state;
 }
 
@@ -141,17 +140,27 @@ enum Statetype handle_in_comment(int currentchar) {
    return state;
 }
 
-
+/* Body of program, while loop reads every char until the bottom of
+the file, tracks the line number, tracks the state at end (accept or
+reject) */
 int main(void){
+   /* Tracks the current char being fed into the program, prompting
+   the state swithces */
    int currentchar;
+   /* Tracks the current line, incrementing as \n encountered */
    int current_line = 1;
+   /* Tracks the number of comments in an unterminated comment that
+   must not be counted in the error message's final line count */
    int inner_comment_line = 0;
+
    enum Statetype state = normal_text;
 
+   /* Central while loop drives the 'reading' of the file */
    while ((currentchar = getchar()) != EOF) {
       if (currentchar == '\n') {
             current_line++;
          }
+         /* Transitions between states based on the read char */
       switch (state) {
          case normal_text:
             state = handle_normal_text(currentchar);
@@ -171,26 +180,24 @@ int main(void){
          case potential_comment:
             state = handle_potential_comment(currentchar);
             break; 
-         case in_comment:
-         if (inner_comment_line == 0) {  
+         case in_comment: 
             inner_comment_line = current_line;
-         }
             state = handle_in_comment(currentchar);
             break; 
          case potential_comment_end:
-         if (inner_comment_line == 0) { 
             inner_comment_line = current_line;
-         }
             state = handle_potential_comment_end(currentchar);
             break;
       }
    }
-
+   /* Deals with edge case: ending on potential comment,
+    / not printed at end */
    if (state == potential_comment) {
       putchar('/');
    }
 
-
+   /* Determines final state (accept or reject, provides error
+   message with specified line number in case of rejection */
    if (state == in_comment || state == potential_comment_end) {
       /*if (currentchar == '\n') {
          inner_comment_line++; }*/
